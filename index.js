d9fc573e4976e01f0b89f8333f3f238ddaa65eee
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-01-12 13:36:55 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(arg = 'roller-skate') {
+    return `This Saturday, I want to ${arg}!`;
+}
+
+function mondayWork(arg = 'go to the office') {
+    return `This Monday, I will ${arg}.`;
+}
+
+function wrapAdjective(wrap = "*") {
+    return function(str = "special") {
+        return "You are " + wrap + str + wrap + "!";
+    };
+}
